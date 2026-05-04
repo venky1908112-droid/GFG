007a817990ed6d34e4eb0c316dcb64cc15d92d0c
Venkat <venky1908112@gmail.com>
2026-05-04 11:08:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Binary/palindrome-binary.py
+++ Difficulty: Easy/Palindrome Binary/palindrome-binary.py
@@ -1,6 +1,11 @@
-#User function Template for python3
-
 class Solution:
-    def isPallindrome(self, n):
-        n = bin(n)[2:]
-        return 1 if n == n[::-1] else 0
+    def isBinaryPalindrome(self, n):
+        left = n.bit_length() - 1
+        right = 0
+        while left > right:
+            if (n >> left) & 1 != (n >> right) & 1:
+                return 0
+            
+            left -= 1
+            right += 1
+        return 1
